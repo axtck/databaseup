cc573c04755b42588d9f489a839f8ba23498346c
axtck-bomanj <alexandertack@hotmail.be>
2022-01-15 22:54:46 +0100

    feat: update create command

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
+
 import fs from "fs";
 import path from "path";
 import mkdirp from "mkdirp";
 
-import { getMigrationNameFromArgv, handleExceptionLazy } from "../utils";
+import { getCreateArgs, handleExceptionLazy, successText } from "../utils";
 
 const migrationFileContent: string = `import mysql from "mysql2/promise";
 
@@ -18,14 +20,17 @@ export const downgrade = async (connection: mysql.Connection): Promise<void> => 
 
 const write = async (): Promise<void> => {
     try {
-        const migrationName: string = getMigrationNameFromArgv();
-        const migrationsFolder: string = path.resolve(".", "migrations");
+        const migrationName: string = getCreateArgs()[0];
+        const srcFolder: string = getCreateArgs()[1];
+        const migrationsFolder: string = path.resolve(".", srcFolder, "migrations");
         await mkdirp(migrationsFolder);
         const migrationPath = path.resolve(migrationsFolder, `${migrationName}.ts`);
         console.log(migrationPath);
         fs.writeFileSync(migrationPath, migrationFileContent);
     } catch (e) {
         handleExceptionLazy(e, "writing migration failed");
+        console.log(successText("Arg 1: migration name"));
+        console.log(successText("Arg 2: /src folder name"));
     }
 };
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,23 @@
 export const handleExceptionLazy = (e: unknown, message: string): void => {
     if (e instanceof Error) {
-        console.log(`${message}: ${e.message}`);
+        console.log(warningText(`${message}: ${e.message}`));
     } else {
-        console.log(`value thrown was not an error, ${message}: ${e}`);
+        console.log(warningText(`value thrown was not an error, ${message}: ${e}`));
     }
 };
 
-export const getMigrationNameFromArgv = (): string => {
+export const getCreateArgs = (): string[] => {
     const args = process.argv.slice(2);
-    if (!args || !args.length) throw new Error("no migration name arg passed");
-    if (args.length > 1) throw new Error("only one arg can be passed");
-    return args[0];
+    if (!args || !args.length) throw new Error("no args passed");
+    if (args.length < 2) throw new Error("missing arg");
+    if (args.length > 2) throw new Error("too many args passed");
+    return args;
+};
+
+export const successText = (text: string) => {
+    return `\x1b[32m${text}\x1b[0m`;
+};
+
+export const warningText = (text: string) => {
+    return `\x1b[33m${text}\x1b[0m`;
 };
