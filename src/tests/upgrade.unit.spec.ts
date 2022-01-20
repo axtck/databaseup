83ce1f2c00e7ba3b90ebd27b7db66c672626ee44
axtck-bomanj <alexandertack@hotmail.be>
2022-01-20 20:42:33 +0100

    feat: fix upgrade test

--- src/tests/upgrade.unit.spec.ts
+++ src/tests/upgrade.unit.spec.ts
@@ -1,16 +1,19 @@
 import path from "path";
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import { upgradeDatabase } from "../upgrade";
 
 describe("upgrade", () => {
     const migrationsPath = path.resolve(path.join(".", "src", "tests", "migrations"));
-    const connection: mysql.Connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "admin"
-    });
-
+    const createTestConnection = async (): Promise<mysql.Connection> => {
+        const connection: mysql.Connection = await mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "admin"
+        });
+        return connection;
+    };
     it("should succeed", async () => {
+        const connection: mysql.Connection = await createTestConnection();
         await upgradeDatabase(migrationsPath, connection);
     });
 });
